7814b34dabb1fa0dd5de325d5036bf8b0eaa4a71
Kevin Wiegman <wiegmank@gmail.com>
2021-02-21 16:29:57 +0000

    quotation marks

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,17 +3,17 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
+let question = 'Who was the first American woman in space? ';
+let correctAnswer = 'Sally Ride';
+let candidateAnswer = '';
 
-let questions = ["Who was the first American woman in space? ", 
-"True or false: 5000 meters == 5 kilometers? ", 
-"(5 + 3)/2 * 10 = ? ", 
-"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
- "What is the minimum crew size for the ISS? "];
+let questions = ['Who was the first American woman in space? ', 
+'True or false: 5000 meters == 5 kilometers? ', 
+'(5 + 3)/2 * 10 = ? ', 
+'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ',
+ 'What is the minimum crew size for the ISS? '];
 
 let correctAnswers = ["Sally Ride", 
 "true", 
